6313ab577202cd39be7afe0f43ef0dcffef287be
NicolasMartins <104094445+nicolasmartins2907@users.noreply.github.com>
2023-06-06 16:47:21 -0300

    Add files via upload

---
+++ PrimeiroSpring2/src/main/java/com/projeto/primeirospring/PrimeiroSpring2Application.java
@@ -0,0 +1,13 @@
+package com.projeto.primeirospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroSpring2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroSpring2Application.class, args);
+	}
+
+}
---
+++ PrimeiroSpring2/src/test/java/com/projeto/primeirospring/PrimeiroSpring2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.primeirospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroSpring2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
